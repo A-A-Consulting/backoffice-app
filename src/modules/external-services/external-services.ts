7b8dbf3d0df637dbb087ed07fce0624683fe4d01
jeandrader <jandrade@revaicare.com>
2023-02-11 12:01:23 -0300

    correction logs

--- src/modules/external-services/external-services.ts
+++ src/modules/external-services/external-services.ts
@@ -2,12 +2,11 @@
 import axios from "axios"
 
 export const userRecordController = async (user: any) => {
-  //console.log("🚀 ~ file: external-services.ts:16 ~ userRecordController ~ user", user)
   try {
     const response = await axios.post('/users/create', user)
     return response
   } catch (error) {
-    console.log("🚀 ~ file: external-services.ts:10 ~ userRecordController ~ error", error)
+    console.error("🚀 ~ file: external-services.ts:10 ~ userRecordController ~ error", error)
     return null
   }
 }
@@ -15,7 +14,7 @@ export const userRecordController = async (user: any) => {
 export const loging = async (data: any) => {
   try {
     const response = await axios.post('/users/login', data)
-    //    console.log("🚀 ~ file: external-services.ts:8 ~ loging ~ response", response)
+    //    console.error("🚀 ~ file: external-services.ts:8 ~ loging ~ response", response)
     return response
   } catch (error) {
     console.error("🚀 ~ file: external-services.ts:21 ~ loging ~ error", error)
@@ -28,7 +27,7 @@ export const getUsers = async () => {
     const response = await axios.get('/users/')
     return response
   } catch (error) {
-    console.log("🚀 ~ file: external-services.ts:31 ~ getUsers ~ error", error)
+    console.error("🚀 ~ file: external-services.ts:31 ~ getUsers ~ error", error)
     return null
   }
 }
@@ -38,7 +37,7 @@ export const getUserById = async (id: string) => {
     const response = await axios.get(`/users/${id}`)
     return response
   } catch (error) {
-    console.log("🚀 ~ file: external-services.ts:41 ~ getUserById ~ error", error)
+    console.error("🚀 ~ file: external-services.ts:41 ~ getUserById ~ error", error)
     return null
   }
 }
@@ -49,7 +48,7 @@ export const updateUserById = async (user: any) => {
     const response = await axios.put(`/users/${id}`, { account_name, firstName, lastName, email })
     return response
   } catch (error) {
-    console.log("🚀 ~ file: external-services.ts:52 ~ updateUserById ~ error", error)
+    console.error("🚀 ~ file: external-services.ts:52 ~ updateUserById ~ error", error)
     return null
   }
 }
@@ -60,7 +59,7 @@ export const changeStateByAccountName = async (account_name: string, status: str
     const response = await axios.put(`/users/state/${account_name}`, { status })
     return response
   } catch (error) {
-    console.log("🚀 ~ file: external-services.ts:63 ~ changeStateByAccountName ~ error", error)
+    console.error("🚀 ~ file: external-services.ts:63 ~ changeStateByAccountName ~ error", error)
     return null
   }
 }
@@ -71,7 +70,7 @@ export const changeRoleByAccountName = async (account_name: string, access: stri
     const response = await axios.put(`/users/role/${account_name}`, { access })
     return response
   } catch (error) {
-    console.log("🚀 ~ file: external-services.ts:74 ~ changeRoleByAccountName ~ error", error)
+    console.error("🚀 ~ file: external-services.ts:74 ~ changeRoleByAccountName ~ error", error)
     return null
   }
 }
@@ -81,7 +80,7 @@ export const deleteUserById = async (id: string) => {
     const response = await axios.delete(`/users/${id}`) 
     return response
   } catch (error) {
-    console.log("🚀 ~ file: external-services.ts:84 ~ deleteUserById ~ error", error)
+    console.error("🚀 ~ file: external-services.ts:84 ~ deleteUserById ~ error", error)
     return null
   }
 }
